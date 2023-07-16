dbf3f8a2270e8f783cf27a1a238115bbf595a397
Assou <assia.asri@gmail.com>
2023-07-16 18:43:48 +0200

    Afficher les trainings sur la page d'accueil, avec un minimum de style pour la visibilité

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+//import de la librairie axios
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+
+  //afficher la liste des trainings
+  const [listOfTrainings, setListOfTrainings] = useState([]);
+
+  //récupérer les données du backend : la liste des trainings
+  useEffect(()=>{
+    axios.get("http://localhost:3001/trainings").then((response) => {
+      setListOfTrainings(response.data);
+    });
+  },[]);
+
   return (
     <div className="App">
-      
+      {listOfTrainings.map((value, key) => {
+        return <div className="training">
+          <div className="trainingPicture">
+            <img src={value.trainingPicture}/>
+            </div>
+          <div className="trainingName">{value.trainingName}</div>
+          <div className="trainingSlogan">{value.trainingSlogan}</div>
+          </div>
+      })}
     </div>
   );
 }
