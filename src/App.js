96b8c2f33c758ef7c7b94171950f0a4e611763d5
Assou <assia.asri@gmail.com>
2023-07-17 21:09:52 +0200

    affichage des programmes dans l'accueil

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [programs, setPrograms] = useState([])
+
+  // ce hook s'execute une seule fois grace au tableau vide (deuxieme parametre)
+  useEffect(() => {
+    fetch('http://localhost:3001/programs').then((response) => {
+      return response.json()
+    }).then((data) => {
+      setPrograms(data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='table'>
+        <div className='table-row'>
+          <div className='table-column'>
+            libellé
+          </div>
+          <div className='table-column'>
+            slogan
+          </div>
+          <div className='table-column'>
+            description
+          </div>
+          <div className='table-column'>
+            url
+          </div>
+          <div className='table-column'>
+            prix
+          </div>
+          <div className='table-column'>
+            crée le
+          </div>
+        </div>
+        {
+          programs.map((program) => {
+            return <div className='table-row'>
+              <div className='table-column'>{program.name}</div>
+              <div className='table-column'>{program.slogan}</div>
+              <div className='table-column'>{program.description}</div>
+              <div className='table-column'>{program.url}</div>
+              <div className='table-column'>{program.price}</div>
+              <div className='table-column'>{program.createdAt}</div>
+            </div>
+          })
+        }
+      </div>
     </div>
   );
 }
